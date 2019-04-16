9f989352fe1d697b996e9eb9f97f6e42ed7d0b19
zhaopin <zhaopin.impl@gmail.com>
2019-04-16 22:07:31 +0800

    modify highlight style

---
+++ public/javascripts/code_style.js
@@ -0,0 +1,14 @@
+
+
+Array.from(document.querySelectorAll('pre code')).forEach(function(node) {
+    var lines = node.innerText.split('\n').length - 1
+    var $numbering = document.createElement('ul')
+    $numbering.className = 'pre-numbering'
+    node.className = 'has-numbering'
+    node.parentNode.appendChild($numbering)
+    for (var i = 1; i < lines; i++) {
+        var num = document.createElement('li')
+        num.innerText = i + ''
+        $numbering.append(num)
+    }
+})
